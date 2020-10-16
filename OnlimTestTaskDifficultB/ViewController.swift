e604eb4314ad818ea92e0d5c3286fa45095950f2
IrelDev <ireldeveloper@gmail.com>
2020-10-16 18:03:59 +0500

    Initial Commit

---
+++ OnlimTestTaskDifficultB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlimTestTaskDifficultB
+//
+//  Created by Kirill Pustovalov on 16.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
