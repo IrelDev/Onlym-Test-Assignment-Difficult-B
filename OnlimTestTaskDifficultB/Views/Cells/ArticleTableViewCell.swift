b9ef8f07dcbc1bf4b33ef9769e34c317f80b0b81
IrelDev <ireldeveloper@gmail.com>
2020-10-16 23:16:38 +0500

    Configure ArticleTableViewCell

--- OnlimTestTaskDifficultB/Views/Cells/ArticleTableViewCell.swift
+++ OnlimTestTaskDifficultB/Views/Cells/ArticleTableViewCell.swift
@@ -8,16 +8,65 @@
 import UIKit
 
 class ArticleTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private var title: String = "" {
+        didSet {
+            titlelabel.text = title
+        }
+    }
+    private var descriptionText: String = "" {
+        didSet {
+            descriptionTextlabel.text = descriptionText
+        }
+    }
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .leading
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    private let titlelabel: UILabel = {
+        let titlelabel = UILabel()
+        titlelabel.font = titlelabel.font.withSize(25)
+        titlelabel.translatesAutoresizingMaskIntoConstraints = false
+        return titlelabel
+    }()
+    private let descriptionTextlabel: UILabel = {
+        let descriptionTextlabel = UILabel()
+        descriptionTextlabel.numberOfLines = 0
+        descriptionTextlabel.font = descriptionTextlabel.font.withSize(20)
+        descriptionTextlabel.textColor = .secondaryLabel
+        descriptionTextlabel.translatesAutoresizingMaskIntoConstraints = false
+        return descriptionTextlabel
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupCell()
+    }
+    private func setupCell() {        
+        stackView.addArrangedSubview(titlelabel)
+        stackView.addArrangedSubview(descriptionTextlabel)
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+                    stackView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor, constant: 0.0),
+            stackView.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor, constant: 0.0),
+            stackView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor, constant: 0.0),
+            stackView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor, constant: 0.0),
+                    ])
+    }
+    func setupCellData(title: String, text: String) {
+        self.title = title
+        self.descriptionText = text
     }
 
 }
--- OnlimTestTaskDifficultB/Views/HomeViewController.swift
+++ OnlimTestTaskDifficultB/Views/HomeViewController.swift
@@ -49,18 +49,17 @@ class HomeViewController: UIViewController {
         bannersCollectionView.register(BannerCollectionViewCell.self, forCellWithReuseIdentifier: "bannersCollectionViewCell")
         
         view.addSubview(articleTableView)
-        
         articleTableView.delegate = self
         articleTableView.dataSource = self
         
-        articleTableView.register(ArticleTableViewCell.self, forCellReuseIdentifier: "articleTableViewCell")
+        articleTableView.register(ArticleTableViewCell.self, forCellReuseIdentifier: "articlesTableViewCell")
     }
     func setupAutoLayout() {
         NSLayoutConstraint.activate([
             bannersCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             bannersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             bannersCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            bannersCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3)
+            bannersCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25)
         ])
         NSLayoutConstraint.activate([
             articleTableView.topAnchor.constraint(equalTo: bannersCollectionView.bottomAnchor, constant: 10),
@@ -75,22 +74,27 @@ class HomeViewController: UIViewController {
         
     }
 }
+// MARK: - UITableViewExtensions
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         20
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "AAA"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "articlesTableViewCell", for: indexPath as IndexPath) as! ArticleTableViewCell
+        if indexPath.row == 2 {
+            cell.setupCellData(title: "Title + \(indexPath.row)", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla arcu a ipsum iaculis placerat. Vestibulum quis interdum neque. Nullam diam dui, fermentum ut tempus venenatis, volutpat vitae leo. Nulla facilisi. Aliquam convallis id urna sed vestibulum. Nunc nec mauris eleifend, feugiat lacus vitae, consequat nisi. Suspendisse gravida, metus sed tincidunt fringilla, odio velit ornare augue, id dapibus neque odio in enim. Phasellus id felis purus. Proin porttitor eros eu arcu hendrerit venenatis. Nullam feugiat egestas tortor, vel sollicitudin ante ultricies eget. Nunc malesuada et nibh vel sollicitudin. \n Morbi dapibus, elit eget rhoncus consectetur, arcu dui dignissim turpis, ac convallis ante metus ut tortor. Nunc et hendrerit nibh, quis ornare sapien. Maecenas imperdiet eget tortor sagittis maximus. Duis augue sapien, commodo ac maximus vitae, gravida eget odio. Suspendisse iaculis leo et mi mollis vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam fringilla malesuada maximus. Etiam ultricies, neque ac tristique rhoncus, urna enim lobortis elit, porttitor dictum mauris enim ut libero. Sed augue velit, interdum faucibus lectus non, auctor mattis dolor. Fusce pellentesque ornare est sed ultrices. Suspendisse finibus mi fermentum turpis scelerisque, et mattis erat lacinia. Aenean augue urna, blandit eget aliquet ut, hendrerit nec quam.\n Phasellus aliquet porttitor eros, eu ullamcorper odio ultricies sit amet. Praesent placerat nibh quis nulla iaculis, eu maximus tortor ultricies. Vivamus et elit sed nulla porta ultricies ut blandit elit. Praesent molestie ac risus nec euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut eget elit quis ipsum accumsan dictum. Cras purus erat, faucibus in nisi non, tempor laoreet sapien.")
+        } else {
+            cell.setupCellData(title: "Title + \(indexPath.row)", text: "Hi bitch")
+        }
         return cell
     }
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
         
         let label = UILabel()
-        label.font = label.font.withSize(25)
+        label.font = label.font.withSize(30)
         
-        label.frame = CGRect.init(x: 5, y: 5, width: headerView.frame.width-10, height: headerView.frame.height-10)
+        label.frame = CGRect.init(x: 5, y: 5, width: headerView.frame.width - 10, height: headerView.frame.height - 10)
         label.text = "Заголовок"
         headerView.addSubview(label)
         
@@ -99,13 +103,20 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 50
     }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
+    }
 }
 extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         5
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = bannersCollectionView.dequeueReusableCell(withReuseIdentifier: "bannersCollectionViewCell", for: indexPath as IndexPath) as! BannerCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bannersCollectionViewCell", for: indexPath as IndexPath) as! BannerCollectionViewCell
         cell.setupCellData(name: "Баннер", color: "#0000ff", active: true)
         return cell
     }
