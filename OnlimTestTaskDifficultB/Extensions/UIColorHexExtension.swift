d86c0bfec5da0c90ace0a10389a71c1787771396
IrelDev <ireldeveloper@gmail.com>
2020-10-16 21:35:30 +0500

    Add UIColorHex extension

---
+++ OnlimTestTaskDifficultB/Extensions/UIColorHexExtension.swift
@@ -0,0 +1,31 @@
+//
+//  UIColorHexExtension.swift
+//  OnlimTestTaskDifficultB
+//
+//  Created by Kirill Pustovalov on 16.10.2020.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  convenience init(_ hex: String, alpha: CGFloat = 1.0) {
+    var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+    
+    if (cString.hasPrefix("#")) { cString.removeFirst() }
+    
+    if ((cString.count) != 6) {
+        self.init("ff0000") // return red color for wrong hex input
+      return
+    }
+    
+    var rgbValue: UInt64 = 0
+    Scanner(string: cString).scanHexInt64(&rgbValue)
+    
+    self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+              green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+              blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+              alpha: alpha)
+  }
+
+}
