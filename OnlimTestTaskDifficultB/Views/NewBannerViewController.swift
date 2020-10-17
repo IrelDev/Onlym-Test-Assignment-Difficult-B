e695f2c5cf4f1632ebe8c44bac8105c269a9ead8
IrelDev <ireldeveloper@gmail.com>
2020-10-17 11:26:53 +0500

    Add new ViewControllers

---
+++ OnlimTestTaskDifficultB/Views/NewBannerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewBannerViewController.swift
+//  OnlimTestTaskDifficultB
+//
+//  Created by Kirill Pustovalov on 17.10.2020.
+//
+
+import UIKit
+
+class NewBannerViewController: UIViewController {
+    var onSaveClosure: (() -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
