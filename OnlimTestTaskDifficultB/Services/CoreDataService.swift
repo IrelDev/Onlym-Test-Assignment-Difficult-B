2a4a783be71698ed9988ca1fca2f590d61cd3b1a
IrelDev <ireldeveloper@gmail.com>
2020-10-17 08:37:32 +0500

    Add CoreDataService

---
+++ OnlimTestTaskDifficultB/Services/CoreDataService.swift
@@ -0,0 +1,57 @@
+//
+//  CoreDataService.swift
+//  OnlimTestTaskDifficultB
+//
+//  Created by Kirill Pustovalov on 17.10.2020.
+//
+
+import UIKit
+import CoreData
+
+struct CoreDataService {
+    static let shared = CoreDataService()
+    public init() { }
+    
+    private func fetchHomeModelData() -> NSData? {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return nil }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "HomeModelEntity")
+        
+        do {
+            let fetchRequestResults = try managedContext.fetch(fetchRequest)
+            
+            let data = fetchRequestResults.first?.value(forKey: "data") as? NSData
+            return data
+            
+        } catch {
+            print("could not fetch data \(error.localizedDescription)")
+        }
+        return nil
+    }
+    private func saveHomeModelData(data: NSData) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "HomeModelEntity", in: managedContext)!
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "HomeModelEntity")
+        
+        do {
+            let fetchRequestResults = try managedContext.fetch(fetchRequest)
+            if fetchRequestResults.count == 0 {
+                let nsmanagedObject = NSManagedObject(entity: entity, insertInto: managedContext)
+                
+                nsmanagedObject.setValue(data, forKeyPath: "data")
+            } else {
+                let previousResults = fetchRequestResults.first!
+                
+                previousResults.setValue(data, forKey: "data")
+            }
+            try managedContext.save()
+        } catch {
+            print("Error, could not save data \(error.localizedDescription)")
+        }
+    }
+}
+
+
